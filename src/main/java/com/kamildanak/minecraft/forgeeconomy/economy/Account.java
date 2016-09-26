b9f5b7d66eecc156de9d33f9cb4ceea64ff117a1
Kamil Danak <kamildanak@gmail.com>
2016-09-26 10:27:30 +0200

    Deleted pin

--- src/main/java/com/kamildanak/minecraft/forgeeconomy/economy/Account.java
+++ src/main/java/com/kamildanak/minecraft/forgeeconomy/economy/Account.java
@@ -7,12 +7,11 @@ import com.kamildanak.minecraft.forgeeconomy.network.PacketDispatcher;
 import com.kamildanak.minecraft.forgeeconomy.network.client.MessageBalance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import org.apache.commons.codec.binary.Hex;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.security.*;
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -23,21 +22,23 @@ public class Account {
 
     private UUID uuid;
     private long balance;
-    private int dailyLimit;
-    private int transactionLimit;
-    private int noPinLimit;
-    private String pin;
+
+    private Account(UUID uuid) {
+        this.uuid = uuid;
+        this.balance = 0;
+        this.changed = true;
+    }
 
     public static Account get(EntityPlayer player) {
         return get(player.getUniqueID());
     }
 
-    private static Account get(UUID uuid)
-    {
+    private static Account get(UUID uuid) {
         Account account = objects.get(uuid.toString());
-        if(account!=null) return account;
+        if (account != null) return account;
 
         if (location == null) return null;
+        //noinspection ResultOfMethodCallIgnored
         location.mkdirs();
 
         account = new Account(uuid);
@@ -54,19 +55,22 @@ public class Account {
         return account;
     }
 
-    private File getFile() {
-        return new File(location, uuid + ".json");
+    public static void writeAll() throws IOException {
+        for (Account account : objects.values())
+            if (account.changed) account.write();
     }
 
-    private Account(UUID uuid)
-    {
-        this.uuid = uuid;
-        this.balance = 0;
-        this.dailyLimit = 0;
-        this.transactionLimit = 0;
-        this.noPinLimit = 0;
-        this.pin = "";
-        this.changed = true;
+    public static void clear() {
+        Account.location = null;
+        Account.objects.clear();
+    }
+
+    public static void setLocation(File location) {
+        Account.location = location;
+    }
+
+    private File getFile() {
+        return new File(location, uuid + ".json");
     }
 
     private void read() throws IOException {
@@ -82,10 +86,6 @@ public class Account {
             Object obj = jsonParser.parse(new FileReader(file));
             JsonObject jsonObject = (JsonObject) obj;
             balance = jsonObject.get("balance").getAsLong();
-            dailyLimit = jsonObject.get("dailyLimit").getAsInt();
-            transactionLimit = jsonObject.get("transactionLimit").getAsInt();
-            noPinLimit = jsonObject.get("noPinLimit").getAsInt();
-            pin = jsonObject.get("pin").getAsString();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -99,10 +99,6 @@ public class Account {
     private void write(File location) throws IOException {
         JsonObject obj = new JsonObject();
         obj.addProperty("balance", balance);
-        obj.addProperty("dailyLimit", dailyLimit);
-        obj.addProperty("transactionLimit", transactionLimit);
-        obj.addProperty("noPinLimit", noPinLimit);
-        obj.addProperty("pin", pin);
         try (FileWriter file = new FileWriter(location)) {
             String str = obj.toString();
             file.write(str);
@@ -110,67 +106,21 @@ public class Account {
         changed = false;
     }
 
-    public static void writeAll() throws IOException {
-        for (Account account : objects.values())
-            if (account.changed) account.write();
-    }
-
-    public static void clear(){
-        Account.location = null;
-        Account.objects.clear();
-    }
-
-    public static void setLocation(File location)
-    {
-        Account.location = location;
-    }
-
-
-    public int getDailyLimit(){
-        return dailyLimit;
-    }
-
-    public int getTransactionLimit(){
-        return transactionLimit;
-    }
-
-    public long getBalance(){
+    public long getBalance() {
         return balance;
     }
-    public void addBalance(long v) {
-        setBalance(balance + v);
-    }
-    public void setBalance(long v){
+
+    public void setBalance(long v) {
         balance = v;
         changed = true;
         PacketDispatcher.sendTo(new MessageBalance(balance), getPlayerMP());
     }
 
-    public int getNoPinLimit() { return noPinLimit; }
-
-    public boolean checkPin(String pinToCheck)
-    {
-        try {
-            return pin.equals(getMD5(pinToCheck));
-        } catch (NoSuchAlgorithmException e) {
-            return false;
-        }
+    public void addBalance(long v) {
+        setBalance(balance + v);
     }
 
-    public void setPin(String newPin) throws NoSuchAlgorithmException {
-        pin = getMD5(newPin);
-    }
-
-    private EntityPlayerMP getPlayerMP()
-    {
+    private EntityPlayerMP getPlayerMP() {
         return ForgeEconomy.minecraftServer.getPlayerList().getPlayerByUUID(uuid);
-    }
-
-    private String getMD5(String string) throws NoSuchAlgorithmException {
-        final MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        messageDigest.reset();
-        messageDigest.update(string.getBytes(Charset.forName("UTF8")));
-        final byte[] resultByte = messageDigest.digest();
-        return new String(Hex.encodeHex(resultByte));
     }
 }
